2e22fe7932328674be7f817b5a68c619c0ef2bd1
Pei LUO <silenceluo@gmail.com>
2019-08-12 16:39:23 -0700

    Change tab to space

--- systemc_tutorial/counter.cpp
+++ systemc_tutorial/counter.cpp
@@ -1,26 +1,26 @@
 #include "systemc.h"
 
 SC_MODULE(first_counter) {
-	sc_in_clk 	clk;
-	sc_in<bool>	reset;
-	sc_in<bool>	enable;
-	sc_out<sc_unit<4>> 	counter_out;
-	
-	sc_unit<4> 	count;
-	
-	void incr_count() {
-		if(reset.read() == 1) {
-			count = 0;
-			counter_out.write(count);
-		} else if(enable.read() == 1) {
-			count = count + 1;
-			counter_out.write(count);
-		}
-	}
-	
-	SC_CTOR(first_counter) {
-		SC_METHOD(incr_count);
-		sensitive << reset;
-		sensitive << clock.pos();
-	}
+    sc_in_clk     clk;
+    sc_in<bool>    reset;
+    sc_in<bool>    enable;
+    sc_out<sc_unit<4>>     counter_out;
+    
+    sc_unit<4>     count;
+    
+    void incr_count() {
+        if(reset.read() == 1) {
+            count = 0;
+            counter_out.write(count);
+        } else if(enable.read() == 1) {
+            count = count + 1;
+            counter_out.write(count);
+        }
+    }
+    
+    SC_CTOR(first_counter) {
+        SC_METHOD(incr_count);
+        sensitive << reset;
+        sensitive << clock.pos();
+    }
 }
